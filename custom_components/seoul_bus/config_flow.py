ce0f1243b5721a842c9fcfeb5d8326df1a23b421
Murian <y2k001hj@gmail.com>
2026-03-17 20:45:18 +0900

    Update config_flow.py

--- custom_components/seoul_bus/config_flow.py
+++ custom_components/seoul_bus/config_flow.py
@@ -3,7 +3,7 @@ from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.const import CONF_API_KEY
 from homeassistant.helpers import selector
-from .const import DOMAIN, CONF_STATION_ID, CONF_STATION_NAME, CONF_START_TIME, CONF_END_TIME, CONF_API_ISSUED_DATE
+from .const import DOMAIN, CONF_STATION_ID, CONF_STATION_NAME, CONF_START_TIME, CONF_END_TIME, CONF_INCLUDE_BUSES
 
 class SeoulBusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -12,9 +12,10 @@ class SeoulBusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             await self.async_set_unique_id(user_input[CONF_STATION_ID])
             self._abort_if_unique_id_configured()
-            
-            title = user_input.get(CONF_STATION_NAME) or f"정류장 {user_input[CONF_STATION_ID]}"
-            return self.async_create_entry(title=title, data=user_input)
+            return self.async_create_entry(
+                title=user_input.get(CONF_STATION_NAME) or f"정류장 {user_input[CONF_STATION_ID]}", 
+                data=user_input
+            )
 
         return self.async_show_form(
             step_id="user",
@@ -22,15 +23,13 @@ class SeoulBusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Required(CONF_API_KEY): str,
                 vol.Required(CONF_STATION_ID): str,
                 vol.Optional(CONF_STATION_NAME): str,
-                # 시/분만 입력받는 가장 안전한 방법
                 vol.Optional(CONF_START_TIME, default="00:00"): selector.TextSelector(
                     selector.TextSelectorConfig(type=selector.TextSelectorType.TIME)
                 ),
                 vol.Optional(CONF_END_TIME, default="00:00"): selector.TextSelector(
                     selector.TextSelectorConfig(type=selector.TextSelectorType.TIME)
                 ),
-                vol.Optional("include_buses"): str,
-                vol.Optional(CONF_API_ISSUED_DATE): str,
+                vol.Optional(CONF_INCLUDE_BUSES): str,
             }),
         )
 
@@ -48,20 +47,18 @@ class SeoulBusOptionsFlowHandler(config_entries.OptionsFlow):
             return self.async_create_entry(title="", data=user_input)
 
         conf = {**self._config_entry.data, **self._config_entry.options}
-        
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
                 vol.Required(CONF_API_KEY, default=conf.get(CONF_API_KEY, "")): str,
                 vol.Required(CONF_STATION_ID, default=conf.get(CONF_STATION_ID, "")): str,
                 vol.Optional(CONF_STATION_NAME, default=conf.get(CONF_STATION_NAME, "")): str,
-                vol.Optional(CONF_START_TIME, default=conf.get(CONF_START_TIME, "00:00")[:5]): selector.TextSelector(
+                vol.Optional(CONF_START_TIME, default=conf.get(CONF_START_TIME, "00:00")): selector.TextSelector(
                     selector.TextSelectorConfig(type=selector.TextSelectorType.TIME)
                 ),
-                vol.Optional(CONF_END_TIME, default=conf.get(CONF_END_TIME, "00:00")[:5]): selector.TextSelector(
+                vol.Optional(CONF_END_TIME, default=conf.get(CONF_END_TIME, "00:00")): selector.TextSelector(
                     selector.TextSelectorConfig(type=selector.TextSelectorType.TIME)
                 ),
-                vol.Optional("include_buses", default=conf.get("include_buses", "")): str,
-                vol.Optional(CONF_API_ISSUED_DATE, default=conf.get(CONF_API_ISSUED_DATE, "")): str,
+                vol.Optional(CONF_INCLUDE_BUSES, default=conf.get(CONF_INCLUDE_BUSES, "")): str,
             }),
         )
