9924cbadbbe79d20d8c35544dae1dbc5ce299752
Murian <y2k001hj@gmail.com>
2026-03-17 12:32:29 +0900

    Update config_flow.py

--- custom_components/seoul_bus/config_flow.py
+++ custom_components/seoul_bus/config_flow.py
@@ -2,6 +2,7 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.const import CONF_API_KEY, CONF_NAME
+from homeassistant.helpers import selector
 from .const import DOMAIN, CONF_STATION_ID, CONF_START_TIME, CONF_END_TIME, CONF_API_ISSUED_DATE, DEFAULT_NAME
 
 class SeoulBusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -16,8 +17,13 @@ class SeoulBusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required(CONF_API_KEY): str,
+                # API 키를 별표(****)로 숨김 처리
+                vol.Required(CONF_API_KEY): selector.TextSelector(
+                    selector.TextSelectorConfig(type=selector.TextSelectorType.PASSWORD)
+                ),
                 vol.Required(CONF_STATION_ID): str,
+                vol.Optional("include_buses", default=""): str,
+                vol.Optional("exclude_buses", default=""): str,
                 vol.Optional(CONF_API_ISSUED_DATE): str,
                 vol.Optional(CONF_START_TIME, default="00:00"): str,
                 vol.Optional(CONF_END_TIME, default="23:59"): str,
@@ -32,18 +38,24 @@ class SeoulBusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
 class SeoulBusOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry):
-        self.config_entry = config_entry
+        # self.config_entry 필드 충돌 에러 수정을 위해 변수명 변경
+        self._custom_config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
-        options = self.config_entry.options
-        data = self.config_entry.data
+        options = self._custom_config_entry.options
+        data = self._custom_config_entry.data
+        
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
-                vol.Required(CONF_API_KEY, default=options.get(CONF_API_KEY, data.get(CONF_API_KEY))): str,
+                vol.Required(CONF_API_KEY, default=options.get(CONF_API_KEY, data.get(CONF_API_KEY))): selector.TextSelector(
+                    selector.TextSelectorConfig(type=selector.TextSelectorType.PASSWORD)
+                ),
+                vol.Optional("include_buses", default=options.get("include_buses", data.get("include_buses", ""))): str,
+                vol.Optional("exclude_buses", default=options.get("exclude_buses", data.get("exclude_buses", ""))): str,
                 vol.Optional(CONF_API_ISSUED_DATE, default=options.get(CONF_API_ISSUED_DATE, data.get(CONF_API_ISSUED_DATE, ""))): str,
                 vol.Required(CONF_START_TIME, default=options.get(CONF_START_TIME, data.get(CONF_START_TIME, "00:00"))): str,
                 vol.Required(CONF_END_TIME, default=options.get(CONF_END_TIME, data.get(CONF_END_TIME, "23:59"))): str,
