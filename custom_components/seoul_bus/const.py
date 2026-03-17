ce9b3f32c3c9559761aeab1db72225d665c0773d
Murian <y2k001hj@gmail.com>
2026-03-17 15:59:44 +0900

    Update const.py

--- custom_components/seoul_bus/const.py
+++ custom_components/seoul_bus/const.py
@@ -4,4 +4,4 @@ CONF_STATION_NAME = "station_name"
 CONF_START_TIME = "start_time"
 CONF_END_TIME = "end_time"
 CONF_API_ISSUED_DATE = "api_issued_date"
-DEFAULT_NAME = "" # 기본값을 빈칸으로 수정
+DEFAULT_NAME = "" # 기본값을 빈칸으로 설정
