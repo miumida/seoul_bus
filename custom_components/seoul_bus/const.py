b3754150d75b50bd03fd1e2739aebe1190a448d8
Murian <y2k001hj@gmail.com>
2026-03-17 15:04:12 +0900

    Update const.py

--- custom_components/seoul_bus/const.py
+++ custom_components/seoul_bus/const.py
@@ -1,6 +1,6 @@
 DOMAIN = "seoul_bus"
 CONF_STATION_ID = "station_id"
-CONF_STATION_NAME = "station_name" # 추가
+CONF_STATION_NAME = "station_name"
 CONF_START_TIME = "start_time"
 CONF_END_TIME = "end_time"
 CONF_API_ISSUED_DATE = "api_issued_date"
